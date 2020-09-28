bee5652d739c8f879056e8a91b6d62a002c5761d
iambulldog <mommam3937@gmail.com>
2020-09-28 19:43:17 +0700

    db

--- connectdb.php
+++ connectdb.php
@@ -5,13 +5,13 @@ date_default_timezone_set("Asia/Bangkok");
 	error_reporting(~0);
 
 $serverName  = "localhost";
-//$userName	  = "root";
-//$Password	  = "12345678";
-//$dbName	  = "hostels";
+$userName	  = "u913926455_01";
+$Password	  = "Aa123654";
+$dbName	  = "u913926455_01";
 
-$userName	  = "root";
-$Password	  = "";
-$dbName	  = "ananyalipe_01";
+// $userName	  = "root";
+// $Password	  = "";
+// $dbName	  = "ananyalipe_01";
 
 	$con = mysqli_connect($serverName,$userName,$Password,$dbName);
 
